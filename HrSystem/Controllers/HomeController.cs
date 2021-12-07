00139f49367e17dedbc2a4f35549c9794a180d3c
Eslam Yasser <eslamxyn@gmail.com>
2021-12-07 03:19:57 +0200

    Eslam1

--- HrSystem/Controllers/HomeController.cs
+++ HrSystem/Controllers/HomeController.cs
@@ -1,18 +1,24 @@
 ﻿using HrSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using HrSystem.ViewModels;
+using HrSystem.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HrSystem.Controllers
 {
     public class HomeController : Controller
     {
+        public ApplicationDbContext dbContext;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            this.dbContext = dbContext;
         }
-       
+
         public IActionResult Index()
         {
             return View();
@@ -27,6 +33,15 @@ namespace HrSystem.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GeneralSettings()
+        {
+            GeneralSettingsViewModel viewModel = new GeneralSettingsViewModel();
+            viewModel.extraDiscountSettings = dbContext.ExtraDiscountSettings.ToList();
+            viewModel.weeklyHolidays = dbContext.WeeklyHolidays.ToList();
+
+            return View(viewModel);
         }
     }
 }
---
+++ HrSystem/ViewModels/GeneralSettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HrSystem.Models;
+
+namespace HrSystem.ViewModels
+{
+    public class GeneralSettingsViewModel
+    {
+        public List<ExtraDiscountSetting>? extraDiscountSettings { get; set; }
+        public List<WeeklyHoliday>? weeklyHolidays { get; set; }
+    }
+}

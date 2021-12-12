eda7651a090ed50d0d4a64c1174a51ebf8f08f0d
Mohamed Metwaly <mohamedmetwaly613@gmail.com>
2021-12-12 21:42:12 +0200

    ADD Attendance_ Departure

---
+++ HrSystem/Controllers/AttendancesController.cs
@@ -0,0 +1,74 @@
+﻿using HrSystem.Data;
+using HrSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+
+namespace HrSystem.Controllers
+{
+    public class AttendancesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AttendancesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            var list = new List<Attendance>();
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
+                    var rowcount = worksheet.Dimension.Rows;
+                    for (int row = 2; row < rowcount; row++)
+                    {
+                        list.Add(new Attendance
+                        {
+                            EmployeeId = (worksheet.Cells[row, 1].Value ?? string.Empty).ToString().Trim(),
+                            AttendanceDate = DateTime.Parse((worksheet.Cells[row, 2].Text ?? string.Empty).ToString()),
+                            AttendanceTime = TimeSpan.Parse((worksheet.Cells[row, 3].Text ?? string.Empty).ToString()),
+                            LeaveTime = TimeSpan.Parse((worksheet.Cells[row, 4].Text ?? string.Empty).ToString())
+                        });
+                    }
+                }
+            }
+            foreach (var item in list)
+            {
+                _context.Add(item);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Attendances
+        public IActionResult Index()
+        {
+            ViewBag.EmployeeId = _context.Employees.ToList();
+            return View(_context.Attendances.Include(a => a.Employee).ToList());
+        }
+
+        // POST: Attendances/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AttendanceTime,LeaveTime,AttendanceDate,EmployeeId")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(attendance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendances/CreateXl
+        public IActionResult createXl()
+        {
+            return View();
+        }
+    }
+}

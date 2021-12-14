024871f1865f20123011a54f6ab296381b0e6bcd
Mohamed Metwaly <mohamedmetwaly613@gmail.com>
2021-12-14 02:56:56 +0200

    Edit Attendance

--- HrSystem/Models/Attendance.cs
+++ HrSystem/Models/Attendance.cs
@@ -6,14 +6,15 @@ namespace HrSystem.Models
     public class Attendance
     {
         [Key]
-        public int? AttendanceId { get; set; }
+        public int AttendanceId { get; set; }
         public TimeSpan? OverTime { get; set; }
         public TimeSpan? Late { get; set; }
-        public TimeSpan? AttendanceTime { get; set; }
-        public TimeSpan? LeaveTime { get; set; }
-        public DateTime? AttendanceDate { get; set; }
+        public TimeSpan AttendanceTime { get; set; }
+        public TimeSpan LeaveTime { get; set; }
+        public DateTime AttendanceDate { get; set; }
         [ForeignKey("Employee")]
-        public string? EmployeeId { get; set; }
-        public virtual Employee? Employee { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
     }
 }
